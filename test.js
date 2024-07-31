70e3f2992e28d959b528901cf8194d0716ace8b6
fexlo <626754522@qq.com>
2024-07-31 21:08:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log('==============test=================')
+var electron = require('electron');
+setTimeout(() => {
+  // 显示消息对话框
+    electron.dialog.showMessageBox(mainWindow, {
+        type: 'info',
+        title: 'Information',
+        message: 'Hello from the main process!',
+        buttons: ['OK']
+    }).then(result => {
+        console.log(result.response); // 用户点击的按钮索引
+    });
+  })
